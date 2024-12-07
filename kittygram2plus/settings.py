c32c21493054d43367a4b72ba99ed7808c8863a7
Shamil Salakhov <debilsalakhov@yandex.ru>
2024-12-07 14:57:24 +0300

    add throttling

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -134,6 +134,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '10000/day',
+        'low_request': '1/minute',
+    }
 }
 
 SIMPLE_JWT = {
